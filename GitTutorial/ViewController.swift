15b47835d0253937786586790f3cf5d0ab8452ac
Nehal Ahmed <nehal@Nehals-MacBook-Pro.local>
2019-01-07 16:17:52 +0530

    message

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Nehal Ahmed on 17/10/1940 Saka.
+//  Copyright © 1940 Nehal Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
